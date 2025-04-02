bf83eba2596b0d1cc6eb7e1912c4f4efc1bf5764
CharlesEmmanuelVanier <charles-emmanuel.vanier.1@ens.etsmtl.ca>
2025-04-02 08:43:48 -0400

    First commit on new branch

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import gui
-import orhelperdriver
-import monte_carlo
-import json
-from datetime import datetime
+import orhelper_sim as orhs
 
 #Pas certain de cette ligne (?)
 if __name__ == '__main__':
 
     wind_data = gui.buildGui()
-    print(wind_data.wind_data)
+    Sim = orhs.OpenRocketSimulation(wind_data.wind_data, wind_data.orkfile)
 
    # points = orhelperdriver.LandingPoints()
    # points.print_stats()
---
+++ orhelper_sim.py
@@ -0,0 +1,43 @@
+import os
+import orhelper
+
+class OpenRocketSimulation:
+
+    def __init__(self, wind_data, orkfile):
+
+        self.orkfile = orkfile
+        self.wind_data = wind_data
+
+        def simulation(self):
+
+            try:
+                with orhelper.OpenRocketInstance() as instance:
+                    # Load the document and get simulation
+                    orh = orhelper.Helper(instance)
+
+                    doc = orh.load_doc(self.orkfile)
+                    sim = doc.getSimulation(0)
+
+                    # Randomize various parameters
+                    opts = sim.getOptions()
+
+                    # Run num simulations and add to self
+                    for p in range(self.num_simulations):
+                        print('Running simulation ', p)
+
+                        opts.setWindModelType(instance.wind.WindModelType.MULTI_LEVEL)  # Set multi-level winds
+                        model = opts.getWindModel()
+                        model.clearLevels()
+                        # Loop here to add all the wind levels
+                        model.addWindLevel(500, 1, 90, 0)
+                        model.addWindLevel(750, 5, 90, 0)
+                        orh.run_simulation(sim)
+
+                # After simulation is done, notify and reset UI
+                self.on_simulation_done()
+
+            except Exception as e:
+                print(f"Error during simulation: {e}")
+                messagebox.showerror("Simulation Error", f"An error occurred during the simulation: {e}")
+
+
