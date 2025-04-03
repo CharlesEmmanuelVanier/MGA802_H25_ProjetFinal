7a4864593ce59bfc5ce94889d75e1d00b4f0fd41
CharlesEmmanuelVanier <charles-emmanuel.vanier.1@ens.etsmtl.ca>
2025-04-03 10:35:43 -0400

    J'ai un semblant de sortie de landing position avec une certaine deviation

--- orhelper_sim.py
+++ orhelper_sim.py
@@ -1,5 +1,10 @@
 import os
+from orhelper import FlightDataType
 import orhelper
+from orhelper import FlightDataType, FlightEvent
+from matplotlib import pyplot as plt
+import math
+import numpy as np
 
 class OpenRocketSimulation:
 
@@ -9,7 +14,8 @@ class OpenRocketSimulation:
         self.wind_data = wind_data
         self.ranges = []
         self.bearings = []
-        self.landpoints = []
+        self.apogee = []
+        self.landingpoints = []
 
     def simulation(self):
         i = 0
@@ -17,10 +23,8 @@ class OpenRocketSimulation:
             with orhelper.OpenRocketInstance() as instance:
                 # Load the document and get simulation
                 orh = orhelper.Helper(instance)
-                orh.get_final_values()
                 doc = orh.load_doc(self.ork_file)
                 sim = doc.getSimulation(0)
-
                 # Randomize various parameters
                 opts = sim.getOptions()
 
@@ -34,39 +38,75 @@ class OpenRocketSimulation:
                     # Adding all wind level for a day of simulation
                     for wind in data:
                         model.addWindLevel(wind[0],wind[1],wind[2],wind[3])
-                    #orh.run_simulation(sim)
-                    #Attemps at extracting relevant data
+
+                    #Actual openrocket simulation
+                    orh.run_simulation(sim)
+
 
                     airstarter = AirStart(0)
                     lp = LandingPoint(self.ranges, self.bearings)
                     orh.run_simulation(sim, listeners=(airstarter, lp))
-
-                    self.landpoints.append(lp)
-
+                    self.landingpoints.append(lp)
+                    self.apogee.append(FlightEvent.APOGEE.value)
 
                     i += 1
 
-            # After simulation is done, notify and reset UI
-            #self.on_simulation_done()
 
         except Exception as e:
             print(f"Error during simulation: {e}")
 
-    def extracting_important_data(self):
-        pass
 
-    def on_simulation_done(self, status):
-        pass
+
+    def print_stats(self):
+        print(
+            'Rocket landing zone %3.2f m +- %3.2f m bearing %3.2f deg +- %3.4f deg from launch site. Based on %i simulations.' % \
+            (np.mean(self.ranges), np.std(self.ranges), np.degrees(np.mean(self.bearings)),
+             np.degrees(np.std(self.bearings)), len(self.landingpoints)))
+        print('Flight Apogee', np.mean(self.apogee), 'm')
+
+
+
 
 class LandingPoint(orhelper.AbstractSimulationListener):
     def __init__(self, ranges, bearings):
         self.ranges = ranges
         self.bearings = bearings
 
+    def endSimulation(self, status, simulation_exception):
+        worldpos = status.getRocketWorldPosition()
+        conditions = status.getSimulationConditions()
+        launchpos = conditions.getLaunchSite()
+
+        self.ranges.append(range_flat(launchpos, worldpos))
+        self.bearings.append(bearing_flat(launchpos, worldpos))
+
 class AirStart(orhelper.AbstractSimulationListener):
 
     def __init__(self, altitude):
         self.start_altitude = altitude
+
+    def startSimulation(self, status):
+        position = status.getRocketPosition()
+        position = position.add(0.0, 0.0, self.start_altitude)
+        status.setRocketPosition(position)
+
+
+METERS_PER_DEGREE_LATITUDE = 111325
+METERS_PER_DEGREE_LONGITUDE_EQUATOR = 111050
+
+
+def range_flat(start, end):
+    dy = (end.getLatitudeDeg() - start.getLatitudeDeg()) * METERS_PER_DEGREE_LATITUDE
+    dx = (end.getLongitudeDeg() - start.getLongitudeDeg()) * METERS_PER_DEGREE_LONGITUDE_EQUATOR
+    return math.sqrt(dy * dy + dx * dx)
+
+
+def bearing_flat(start, end):
+    dy = (end.getLatitudeDeg() - start.getLatitudeDeg()) * METERS_PER_DEGREE_LATITUDE
+    dx = (end.getLongitudeDeg() - start.getLongitudeDeg()) * METERS_PER_DEGREE_LONGITUDE_EQUATOR
+    return math.pi / 2 - math.atan(dy / dx)
+
+
 
 
 
